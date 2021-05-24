702d51a77d4328b71464b77b148872b5ab60e42c
Logan Demmy <frank.demmy@gmail.com>
2021-05-24 10:56:16 -0500

    first push

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,11 @@
-// Put your code here
+
+
+const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+"London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+console.log("All Place Names")
+names.forEach(name => console.log(name))
+
+let thenames = names.includes("The")
+
+thenames.forEach(name => console.log())
