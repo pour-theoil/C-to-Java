ace32fab0e98d5311e75cfc432b951dc68106cfb
Logan Demmy <frank.demmy@gmail.com>
2021-05-24 11:28:25 -0500

    dice rolling complete

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,18 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+let rolling =() => {
+    for( let i = 0; i < 10; i++) {
+    let diceone = Math.floor(Math.random() * 6) + 1
+    let dicetwo = Math.floor(Math.random() * 6) + 1
+    if (diceone === dicetwo) {
+        console.log(`${diceone} + ${dicetwo} == ${diceone + dicetwo} DOUBLES!`)
+    }
+    else {
+        console.log(`${diceone} + ${dicetwo} == ${diceone + dicetwo}`)
+    }
+}}
+
+rolling()
